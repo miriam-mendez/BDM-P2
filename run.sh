b91193806b38fd27775751616a19efe4abecb9c5
eliya18 <49730658+eliya18@users.noreply.github.com>
2023-06-15 14:48:42 +0200

    Add files via upload

--- run.sh
+++ run.sh
@@ -2,15 +2,15 @@
 
 echo "======================================================================================================================================================================"
 echo "Running formatted zone and uploading files to HDFS."
-python uploadToFormattedZone.py
+python3 uploadToFormattedZone.py
 echo "Finished formatted zone tasks."
 
 echo "======================================================================================================================================================================"
 echo "Getting data from formatted zone and upload it to exploitation zone..."
-python uploadToExploitationZone.py
+python3 uploadToExploitationZone.py
 echo "Finished creating exploitation zone, KPI's were created."
 
 echo "======================================================================================================================================================================"
 echo "Training ML model..."
-python MLmodel.py
+python3 MLmodel.py
 echo "Model trained has complete and its measures are presented."
